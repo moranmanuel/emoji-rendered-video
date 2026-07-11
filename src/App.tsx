f7d741b38e16cb5efa5fa2310af4838529101543
Manuel Moran <manu.moran0710@gmail.com>
2026-07-11 02:30:54 -0300

    rendered by blocks of pixels instead of px by px

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,105 @@
 import "./App.css"
-import {useRef, useEffect, useState} from 'react'
+import { useRef, useEffect } from 'react'
 
-const emojisData: Array<{emoji:string, r?: number, g?: number, b?: number}> = [{emoji:"🥕"}, {emoji:"🔔"}, {emoji:"🍊"}, {emoji:"💛"}, {emoji:"🖤"}, {emoji:"🤍"}, {emoji:"❤️"}, {emoji:"💜"}, {emoji:"🧡"}, {emoji:"🌸"}, {emoji:"🍋"}, {emoji:"🌊"}, {emoji:"🌙"}, {emoji:"⭐"}]
+const emojisData = [
+    // rojos
+    {emoji:"❤️", r:0, g:0, b:0},
+    {emoji:"🍎", r:0, g:0, b:0},
+    {emoji:"🌹", r:0, g:0, b:0},
+    {emoji:"🍓", r:0, g:0, b:0},
+    // naranjas
+    {emoji:"🥕", r:0, g:0, b:0},
+    {emoji:"🍊", r:0, g:0, b:0},
+    {emoji:"🧡", r:0, g:0, b:0},
+    {emoji:"🦊", r:0, g:0, b:0},
+    // amarillos
+    {emoji:"💛", r:0, g:0, b:0},
+    {emoji:"🌟", r:0, g:0, b:0},
+    {emoji:"🍋", r:0, g:0, b:0},
+    {emoji:"🌻", r:0, g:0, b:0},
+    // verdes
+    {emoji:"🌿", r:0, g:0, b:0},
+    {emoji:"🍀", r:0, g:0, b:0},
+    {emoji:"🥦", r:0, g:0, b:0},
+    {emoji:"🌵", r:0, g:0, b:0},
+    // azules
+    {emoji:"🌊", r:0, g:0, b:0},
+    {emoji:"💙", r:0, g:0, b:0},
+    {emoji:"🫐", r:0, g:0, b:0},
+    {emoji:"🧊", r:0, g:0, b:0},
+    // violetas
+    {emoji:"💜", r:0, g:0, b:0},
+    {emoji:"🍇", r:0, g:0, b:0},
+    {emoji:"🌸", r:0, g:0, b:0},
+    // marrones
+    {emoji:"🪵", r:0, g:0, b:0},
+    {emoji:"🍫", r:0, g:0, b:0},
+    {emoji:"🐻", r:0, g:0, b:0},
+    
+    // negros
+    {emoji:"🖤", r:0, g:0, b:0},
+    {emoji:"🎱", r:0, g:0, b:0},
+    {emoji:"🐈‍⬛", r:0, g:0, b:0},
+    // piel/marron claro
+    {emoji:"🔔", r:0, g:0, b:0},
+    {emoji:"🌾", r:0, g:0, b:0},
+    {emoji:"⭐", r:0, g:0, b:0},
+    {emoji:"👦", r:0, g:0, b:0},
+
+    {emoji:"👧", r:0, g:0, b:0},
+    {emoji:"👨", r:0, g:0, b:0},
+    {emoji:"👩", r:0, g:0, b:0},
+    {emoji:"👴", r:0, g:0, b:0},
+    {emoji:"👵", r:0, g:0, b:0},
+    {emoji:"🧑", r:0, g:0, b:0},
+    {emoji:"👶", r:0, g:0, b:0},
+    {emoji:"🐻", r:0, g:0, b:0},
+    {emoji:"🦊", r:0, g:0, b:0},
+    {emoji:"🐯", r:0, g:0, b:0},
+    {emoji:"🦁", r:0, g:0, b:0},
+    {emoji:"🐮", r:0, g:0, b:0},
+    {emoji:"🐷", r:0, g:0, b:0},
+    {emoji:"🐸", r:0, g:0, b:0},
+    {emoji:"🐵", r:0, g:0, b:0},
+    {emoji:"🦝", r:0, g:0, b:0},
+    {emoji:"🍕", r:0, g:0, b:0},
+    {emoji:"🥩", r:0, g:0, b:0},
+    {emoji:"🍗", r:0, g:0, b:0},
+    {emoji:"🌽", r:0, g:0, b:0},
+    {emoji:"🥑", r:0, g:0, b:0},
+    {emoji:"🫐", r:0, g:0, b:0},
+    {emoji:"🍇", r:0, g:0, b:0},
+    {emoji:"🍉", r:0, g:0, b:0},
+    {emoji:"🥝", r:0, g:0, b:0},
+    {emoji:"🌲", r:0, g:0, b:0},
+    {emoji:"🌴", r:0, g:0, b:0},
+    {emoji:"🍁", r:0, g:0, b:0},
+    {emoji:"🌾", r:0, g:0, b:0},
+    {emoji:"🪨", r:0, g:0, b:0},
+    {emoji:"👑", r:0, g:0, b:0},
+    {emoji:"🏆", r:0, g:0, b:0},
+    {emoji:"🎸", r:0, g:0, b:0},
+    {emoji:"⚽", r:0, g:0, b:0},
+    {emoji:"🏀", r:0, g:0, b:0},
+    {emoji:"🎾", r:0, g:0, b:0},
+    {emoji:"🎱", r:0, g:0, b:0},
+    {emoji:"🌑", r:0, g:0, b:0},
+    {emoji:"🦇", r:0, g:0, b:0},
+    {emoji:"💎", r:0, g:0, b:0},
+    {emoji:"🌀", r:0, g:0, b:0},
+    {emoji:"🐍", r:0, g:0, b:0},
+    {emoji:"🦎", r:0, g:0, b:0},
+    {emoji:"🌱", r:0, g:0, b:0},
+    {emoji:"🥬", r:0, g:0, b:0}
+]
+
+const COLS = 160
 
 export default function App() {
     const videoRef = useRef<HTMLVideoElement>(null)
-    const canvasRef = useRef<HTMLCanvasElement>(null)
-    const canvas2Ref = useRef<HTMLCanvasElement>(null)
-    const canvas3Ref = useRef<HTMLCanvasElement>(null)
+    const videoCanvasRef = useRef<HTMLCanvasElement>(null)
+    const emojiCanvasRef = useRef<HTMLCanvasElement>(null)
+    const renderedCanvasRef = useRef<HTMLCanvasElement>(null)
 
     useEffect(() => {
         navigator.mediaDevices.getUserMedia({ video: true })
@@ -18,22 +110,23 @@ export default function App() {
     }, [])
 
     useEffect(() => {
-        const canvas2 = canvas2Ref.current
-        if (!canvas2) return
-        const ctx2 = canvas2.getContext("2d", { willReadFrequently: true });
-        if (!ctx2) return
-        canvas2.width = 32
-        canvas2.height = 32
+        const emojiCanvas = emojiCanvasRef.current
+        if (!emojiCanvas) return
+        const emojiCanvasCtx = emojiCanvas.getContext("2d", { willReadFrequently: true });
+        if (!emojiCanvasCtx) return
+        emojiCanvas.width = 32
+        emojiCanvas.height = 32
         
         for(const emojiData of emojisData) {
-            ctx2.clearRect(0, 0, canvas2.width, canvas2.height)
-            ctx2.fillText(emojiData.emoji, 0, 0)
-            const canvasData = ctx2.getImageData(0, 0, 32, 32)
+            emojiCanvasCtx.clearRect(0, 0, emojiCanvas.width, emojiCanvas.height)
+            emojiCanvasCtx.fillText(emojiData.emoji, 10, 20)
+            const canvasData = emojiCanvasCtx.getImageData(0, 0, 32, 32)
             const emojiRGBA = canvasData.data
+            
             let rTotal = 0
             let gTotal = 0
             let bTotal = 0
-            let numberOfPixels = 0
+            let numberOfPixels = 0            
 
             for (let i = 0; i < emojiRGBA.length; i+=4) {
                 const alpha = emojiRGBA[i+3]
@@ -49,58 +142,98 @@ export default function App() {
             emojiData.r = rTotal / numberOfPixels
             emojiData.g = gTotal / numberOfPixels
             emojiData.b = bTotal / numberOfPixels
+
+            console.log(emojiData);
         }
     }, [])
 
     useEffect(() => {
         const video = videoRef.current
-        const canvas = canvasRef.current
-        if (!canvas) return
-        const ctx = canvas.getContext("2d", { willReadFrequently: true })
-        const canvas3 = canvas3Ref.current
-        if (!canvas3) return
-        const ctx3 = canvas3.getContext("2d")
-
+        const videoCanvas = videoCanvasRef.current
+        if (!videoCanvas) return
+        const videoCanvasCtx = videoCanvas.getContext("2d", { willReadFrequently: true })
+        const renderedCanvas = renderedCanvasRef.current
+        if (!renderedCanvas) return
+        const renderedCanvasCtx = renderedCanvas.getContext("2d")
+        
         
         function draw() {
-            if (!(video && canvas && canvas3 && ctx && ctx3)) return
+            if (!(video && videoCanvas && renderedCanvas && videoCanvasCtx && renderedCanvasCtx)) return
+            
             if(video.readyState === video.HAVE_ENOUGH_DATA) {
-                canvas.width = video.videoWidth
-                canvas.height = video.videoHeight
-                canvas3.width = video.videoWidth
-                canvas3.height = video.videoHeight
+                videoCanvas.width = video.videoWidth
+                videoCanvas.height = video.videoHeight
+                renderedCanvas.width = video.videoWidth
+                renderedCanvas.height = video.videoHeight
 
-                const emojisGrid = []
-                let emojisLine = []
+                const dividedByPixels = []
+                let dividedByPixelsLine = []
+                const blocks = []
+
+                videoCanvasCtx.drawImage(video, 0, 0)
                 
-                ctx.drawImage(video, 0, 0)
-                
-                const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height)
+                const imageData = videoCanvasCtx.getImageData(0, 0, videoCanvas.width, videoCanvas.height)
 
                 const pixelsRGBA = imageData.data
 
-                for (let i = 0; i < pixelsRGBA.length; i+=4) {
-                    const r = pixelsRGBA[i]
-                    const g = pixelsRGBA[i+1]
-                    const b = pixelsRGBA[i+2]
+                for (let i = 0; i <= pixelsRGBA.length; i+=4) {                    
+                    const currentPixel = {r: pixelsRGBA[i], g: pixelsRGBA[i+1], b: pixelsRGBA[i+2]}
 
-                    const closestEmoji = getClosestEmoji(r, g, b)            
-
-                    if(i / 4 % canvas.width !== 0 || i == 0) emojisLine.push(closestEmoji)
+                    if(i / 4 % videoCanvas.width !== 0 || i == 0) dividedByPixelsLine.push(currentPixel)
                     else {
-                        emojisGrid.push(emojisLine)
-                        emojisLine = []
-                        emojisLine.push(closestEmoji)
+                        dividedByPixels.push(dividedByPixelsLine)
+                        dividedByPixelsLine = []
+                        dividedByPixelsLine.push(currentPixel)
                     }
                 }
 
-                for(let i = 0; i < emojisGrid.length; i++) {
-                    for(let j = 0; j < emojisGrid[i].length; j++) {
-                        const currentEmoji = emojisGrid[i][j]
-                        
-                        if(!currentEmoji) return
+                for (let i = 0; i < videoCanvas.height; i += videoCanvas.width/COLS) {
+                    for (let j = 0; j < videoCanvas.width; j += videoCanvas.width/COLS) {
+                        let rTotalBlock = 0
+                        let gTotalBlock = 0
+                        let bTotalBlock = 0
 
-                        ctx3.fillText(currentEmoji, j, i)}
+                        for (let fila = 0; fila < videoCanvas.width/COLS; fila++) {
+                            for (let columna = 0; columna < videoCanvas.width/COLS; columna++) {
+                                rTotalBlock += dividedByPixels[i+fila][j+columna].r
+                                gTotalBlock += dividedByPixels[i+fila][j+columna].g
+                                bTotalBlock += dividedByPixels[i+fila][j+columna].b
+                            }
+                        }
+
+                        const r = rTotalBlock/((videoCanvas.width/COLS) ** 2)
+                        const g = gTotalBlock/((videoCanvas.width/COLS) ** 2)
+                        const b = bTotalBlock/((videoCanvas.width/COLS) ** 2)
+
+                        const block: {x: number, y: number, r: number, g: number, b: number} = {x: j, y: i, r: r, g: g, b: b}
+                        
+                        blocks.push(block)
+                    }
+                }
+
+                function getClosestEmoji(blockR: number, blockG: number, blockB: number): string {
+                    let closestEmoji = null
+                    let closestDistance = Infinity
+        
+                    for(const emojiData of emojisData) {
+                        const distance = (blockR - emojiData.r) ** 2 + (blockG - emojiData.g) ** 2 + (blockB - emojiData.b) ** 2
+                        
+                        if(distance < closestDistance) {
+                            closestEmoji = emojiData.emoji
+                            closestDistance = distance
+                        }
+                    }
+
+                    if (!closestEmoji) throw new Error("ERROR")
+
+                    return closestEmoji
+                }
+
+                for (const block of blocks) {                    
+                    const closestEmoji = getClosestEmoji(block.r, block.g, block.b)                                        
+                    renderedCanvasCtx.font = `${renderedCanvas.width/COLS}px serif`
+                    renderedCanvasCtx.textBaseline = "top"
+                    renderedCanvasCtx.fillText(closestEmoji, block.x, block.y)
                 }
             }
 
@@ -109,32 +242,17 @@ export default function App() {
 
         draw()
 
-        function getClosestEmoji(pixelR: number, pixelG: number, pixelB: number) {
-            let closestEmoji = null
-            let closestDistance = Infinity
-
-            for(const emojiData of emojisData) {
-                // if(!(emojiData.r && emojiData.g && emojiData.b)) return
-                const distance = (pixelR - emojiData.r) ** 2 + (pixelG - emojiData.g) ** 2 + (pixelB - emojiData.b) ** 2
-                
-                if(distance < closestDistance) {
-                    closestEmoji = emojiData.emoji
-                    closestDistance = distance
-                }
-            }
-            return closestEmoji
-        }
     }, [])
 
     return (
         <div className="container">
             <div className="rendered-video">
-                <canvas ref={canvas3Ref} />
+                <canvas ref={renderedCanvasRef} />
             </div>
             <div className="real-video">
                 <video ref={videoRef} autoPlay />
-                <canvas ref={canvasRef} style={{ display: "none" }} />
-                <canvas ref={canvas2Ref} style={{ display: "none" }} />
+                <canvas ref={emojiCanvasRef} style={{ display: "none" }} />
+                <canvas ref={videoCanvasRef} style={{ display: "none" }} />
             </div>
         </div>
     );
