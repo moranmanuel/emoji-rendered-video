426390854e27f86c644bc37df3a2d1bacb2ce919
Manuel Moran <manu.moran0710@gmail.com>
2026-07-21 17:01:26 -0300

    Dismissed emojis that had too many dark pixels around it

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css"
-import { useRef, useEffect } from 'react'
+import { useRef, useEffect, useState } from 'react'
 import data from 'emoji-datasource/emoji.json'
 
 let emojisData: Array<{emoji: string, r: number, g: number, b: number}> = []
 
-const COLS = 80
+const COLS = 160
+
+const emojiCache = new Map<string, string>();
 
 export default function App() {
     const videoRef = useRef<HTMLVideoElement>(null)
@@ -21,24 +23,30 @@ export default function App() {
     }, [])
 
     useEffect(() => {
+        emojiCache.clear()
+        localStorage.removeItem('emojisData')
         const cached = localStorage.getItem('emojisData')
+        
         if (cached) {
             emojisData = JSON.parse(cached)
         } else {
-            console.log("ENTRE")
             const emojiCanvas = emojiCanvasRef.current
             if (!emojiCanvas) return
             const emojiCanvasCtx = emojiCanvas.getContext("2d", { willReadFrequently: true });
             if (!emojiCanvasCtx) return
             emojiCanvas.width = 32
             emojiCanvas.height = 32
+
+            emojiCanvasCtx.font = "32px sans-serif";
+            emojiCanvasCtx.textAlign = "center";
+            emojiCanvasCtx.textBaseline = "middle";
             
             for (const emoji of data) {
                 const emojiText = String.fromCodePoint(...emoji.unified.split('-').map((u: string) => parseInt(u, 16)))
             
                 emojiCanvasCtx.clearRect(0, 0, emojiCanvas.width, emojiCanvas.height)
-                emojiCanvasCtx.fillText(emojiText, 10, 20)
-                const canvasData = emojiCanvasCtx.getImageData(0, 0, 32, 32)
+                emojiCanvasCtx.fillText(emojiText, 16, 16)
+                const canvasData = emojiCanvasCtx.getImageData(0, 0, emojiCanvas.width, emojiCanvas.height)
                 const emojiRGBA = canvasData.data
                 
                 let rTotal = 0
@@ -57,6 +65,8 @@ export default function App() {
                     }
                 }
 
+                if(numberOfPixels < 600) continue   
+
                 const r = rTotal / numberOfPixels
                 const g = gTotal / numberOfPixels
                 const b = bTotal / numberOfPixels
@@ -64,6 +74,8 @@ export default function App() {
                 const emojiData = {emoji: emojiText, r: r, g: g, b: b}
                 emojisData.push(emojiData)
             }
+
+            console.log(emojisData.length)
 
             localStorage.setItem('emojisData', JSON.stringify(emojisData))
         }
@@ -78,100 +90,98 @@ export default function App() {
         if (!renderedCanvas) return
         const renderedCanvasCtx = renderedCanvas.getContext("2d")
         
-        function draw() {
-            if (!(video && videoCanvas && renderedCanvas && videoCanvasCtx && renderedCanvasCtx)) return
-            
-            if(video.readyState === video.HAVE_ENOUGH_DATA) {
-                videoCanvas.width = video.videoWidth
-                videoCanvas.height = video.videoHeight
-                renderedCanvas.width = video.videoWidth
-                renderedCanvas.height = video.videoHeight
-
-                const longSide = Math.max(videoCanvas.width, videoCanvas.height)
-                const BLOCK_SIZE = longSide / COLS
+        let lastFrame = 0
+        function draw(timestamp: number) {
+            if (timestamp - lastFrame > 66) {
+                lastFrame = timestamp
+                if (!(video && videoCanvas && renderedCanvas && videoCanvasCtx && renderedCanvasCtx)) return
                 
-                const dividedByPixels = []
-                let dividedByPixelsLine = []
-                const blocks = []
+                if(video.readyState === video.HAVE_ENOUGH_DATA) {
+                    videoCanvas.width = video.videoWidth
+                    videoCanvas.height = video.videoHeight
+                    renderedCanvas.width = video.videoWidth
+                    renderedCanvas.height = video.videoHeight
 
-                videoCanvasCtx.drawImage(video, 0, 0)
-                
-                const imageData = videoCanvasCtx.getImageData(0, 0, videoCanvas.width, videoCanvas.height)
+                    const longSide = Math.max(videoCanvas.width, videoCanvas.height)
+                    const BLOCK_SIZE = longSide / COLS
 
-                const pixelsRGBA = imageData.data
+                    videoCanvasCtx.drawImage(video, 0, 0)
+                    
+                    const imageData = videoCanvasCtx.getImageData(0, 0, videoCanvas.width, videoCanvas.height)
 
-                for (let i = 0; i <= pixelsRGBA.length; i+=4) {                    
-                    const currentPixel = {r: pixelsRGBA[i], g: pixelsRGBA[i+1], b: pixelsRGBA[i+2]}
+                    const pixelsRGBA = imageData.data
 
-                    if(i / 4 % videoCanvas.width !== 0 || i == 0) dividedByPixelsLine.push(currentPixel)
-                    else {
-                        dividedByPixels.push(dividedByPixelsLine)
-                        dividedByPixelsLine = []
-                        dividedByPixelsLine.push(currentPixel)
-                    }
-                }
-
-                for (let i = 0; i < videoCanvas.height; i += BLOCK_SIZE) {
-                    for (let j = 0; j < videoCanvas.width; j += BLOCK_SIZE) {
-                        let rTotalBlock = 0
-                        let gTotalBlock = 0
-                        let bTotalBlock = 0
-
-                        for (let fila = 0; fila < BLOCK_SIZE; fila++) {
-                            for (let columna = 0; columna < BLOCK_SIZE; columna++) {
-                                rTotalBlock += dividedByPixels[i+fila][j+columna].r
-                                gTotalBlock += dividedByPixels[i+fila][j+columna].g
-                                bTotalBlock += dividedByPixels[i+fila][j+columna].b
-                            }
-                        }
-
-                        const r = rTotalBlock/((BLOCK_SIZE) ** 2)
-                        const g = gTotalBlock/((BLOCK_SIZE) ** 2)
-                        const b = bTotalBlock/((BLOCK_SIZE) ** 2)
-
-                        const block: {x: number, y: number, r: number, g: number, b: number} = {x: j, y: i, r: r, g: g, b: b}
-                        
-                        blocks.push(block)
-                    }
-                }
-
-                function getClosestEmoji(blockR: number, blockG: number, blockB: number): string {
-                    let closestEmoji = null
-                    let closestDistance = Infinity
-        
-                    for(const emojiData of emojisData) {
-                        const distance = (blockR - emojiData.r) ** 2 + (blockG - emojiData.g) ** 2 + (blockB - emojiData.b) ** 2
-                        
-                        if(distance < closestDistance) {
-                            closestEmoji = emojiData.emoji
-                            closestDistance = distance
-                        }
-                    }
-
-                    if (!closestEmoji) throw new Error("ERROR")
-
-                    return closestEmoji
-                }
-
-                for (const block of blocks) {                    
-                    const closestEmoji = getClosestEmoji(block.r, block.g, block.b)                                        
                     renderedCanvasCtx.font = `${renderedCanvas.width/COLS}px serif`
                     renderedCanvasCtx.textBaseline = "top"
-                    renderedCanvasCtx.fillText(closestEmoji, block.x, block.y)
+
+                    for (let i = 0; i < videoCanvas.height; i += BLOCK_SIZE) {
+                        const row: string[] = []
+                        for (let j = 0; j < videoCanvas.width; j += BLOCK_SIZE) {
+                            let rTotalBlock = 0
+                            let gTotalBlock = 0
+                            let bTotalBlock = 0
+    
+                            for (let fila = 0; fila < BLOCK_SIZE; fila++) {
+                                for (let columna = 0; columna < BLOCK_SIZE; columna++) {
+                                    const index = ((i + fila) * videoCanvas.width + (j + columna)) * 4
+                                    rTotalBlock += pixelsRGBA[index]
+                                    gTotalBlock += pixelsRGBA[index + 1]
+                                    bTotalBlock += pixelsRGBA[index + 2]
+                                }
+                            }
+
+                            const r = rTotalBlock/((BLOCK_SIZE) ** 2)
+                            const g = gTotalBlock/((BLOCK_SIZE) ** 2)
+                            const b = bTotalBlock/((BLOCK_SIZE) ** 2)
+                            
+                            const closestEmoji = getClosestEmoji(r, g, b)
+
+                            renderedCanvasCtx.fillText(closestEmoji, j, i)
+                        }
+                    }
+                    
+                    function getClosestEmoji(blockR: number, blockG: number, blockB: number): string {
+                        let closestEmoji = null
+                        let closestDistance = Infinity
+                        const qr = Math.round(blockR / 8);
+                        const qg = Math.round(blockG / 8);
+                        const qb = Math.round(blockB / 8);
+
+                        const key = `${qr},${qg},${qb}`;
+
+                        const cachedEmoji = emojiCache.get(key);
+
+                        if (cachedEmoji) return cachedEmoji
+                        else {
+                            for(const emojiData of emojisData) {
+                                const distance = (blockR - emojiData.r) ** 2 + (blockG - emojiData.g) ** 2 + (blockB - emojiData.b) ** 2
+                            
+                                if(distance < closestDistance) {
+                                    closestEmoji = emojiData.emoji
+                                    closestDistance = distance
+                                }
+                            }
+
+                            if (!closestEmoji) throw new Error("ERROR")
+                                
+                            emojiCache.set(key, closestEmoji);
+    
+                            return closestEmoji
+                        }   
+                    }
                 }
             }
-
             requestAnimationFrame(draw)
         }
 
-        draw()
-
+        draw(0)
     }, [])
 
     return (
         <div className="container">
             <div className="rendered-video">
                 <canvas ref={renderedCanvasRef} />
+                {/* <canvas ref={emojiCanvasRef} /> */}
             </div>
             <div className="real-video">
                 <video ref={videoRef} autoPlay />
